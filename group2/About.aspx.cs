2c14d426927553e56b6ca3b54df47d62019b8bf5
Kurtis <kurtrobin@outlook.com>
2017-10-31 14:32:06 +0000

    Initial commit

---
+++ group2/About.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace group2
+{
+    public partial class About : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void allDataButt_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("alldata.html");
+        }
+    }
+}
---
+++ group2/Default.aspx
@@ -0,0 +1,11 @@
+﻿<%@  Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="group2._Default" %>
+
+
+
+<asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
+    
+    
+    <a href="alldata2.html" title="All Data">All Data</a>
+    <a href="highestdata.html" title="Highest Data">Highest Data</a>
+    <a href="lowestdata.html" title="Lowest Data">Lowest Data</a>
+</asp:Content>
